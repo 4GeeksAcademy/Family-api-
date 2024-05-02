f5556862c7156ec072fefe3f2c11923e7735ac7d
valeriaasalcedoo <153454325+valeriaasalcedoo@users.noreply.github.com>
2024-05-02 05:18:00 +0000

    donedonedone

--- src/app.py
+++ src/app.py
@@ -25,18 +25,36 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+#Get all members of Jackson's family
 @app.route('/members', methods=['GET'])
-def handle_hello():
+def get_members():
 
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+    return jsonify(members), 200
+
+#GET one member of Jackson's family
+@app.route('/member/<int:member_id>', methods=['GET'])
+def get_one_member(member_id):
+
+    member = jackson_family.get_one_member(member_id)
+    return jsonify(member), 200
 
 
-    return jsonify(response_body), 200
+#POST for add a new member on the Jackson's family
+@app.route('/member', methods=['POST'])
+def post_member():
+
+    info = request.get_json()
+    jackson_family.add_member(info)
+    return jsonify ({"Your new family member has been added successfully"}), 200
+
+#DELETW for remove a member on the Jackson's family
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id):
+
+    member = jackson_family.delete_member(member_id)
+    return jsonify({"done": True}), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
